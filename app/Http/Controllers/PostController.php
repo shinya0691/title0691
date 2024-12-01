be3b38ec9d7c14d30e9d6a8178aa258c9fde13ba
shinya0691 <wshinya515@gmail.com>
2024-12-01 10:10:18 +0000

    適切なコミットメッセージ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
 
-class PostController extends Controller
-{
-    //
+class PostController extends Controller{
+    public function index(Post $post){
+        return $post->get();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; 
+
+
+Route::get('/posts', [PostController::class, 'index']);   
 
 /*
 |--------------------------------------------------------------------------
